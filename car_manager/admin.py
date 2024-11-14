89f4c12f312bd32bcfb8e31f885fac349cb9ab62
SebastianVilches <sivilches@miuandes.cl>
2024-11-14 15:57:22 -0300

    car and message models

--- car_manager/admin.py
+++ car_manager/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Car
+from .models import Message
 
-# Register your models here.
+admin.site.register(Car)
+admin.site.register(Message)
--- car_manager/models.py
+++ car_manager/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Car(models.Model):
+    patente = models.CharField(max_length=20, unique=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.patente} - {self.owner.username}"
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    patente = models.CharField(max_length=20)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    csv = models.FileField(upload_to='uploads/csv_files/')
+
+    def __str__(self):
+        return f"El auto con patente {self.patente} perteneciente al usuario
+                {self.user.username} ha detectado valores extraños {self.csv}, a las {self.timestamp}"
